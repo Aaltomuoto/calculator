bac9300bfb7a5dcf13b9b242e3974af909521559
Jeroen Ophof <j.ophof@gmail.com>
2024-02-07 16:20:07 +0100

    Added basic calculator functions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let fNum,
+    sNum,
+    operator;
+
+const add = function() {
+
+};
+
+const subtract = function() {
+
+};
+
+const multiply = function() {
+
+};
+
+const divide = function() {
+
+};
+
+const operate = function(fNum, sNum, operator) {
+
+};
