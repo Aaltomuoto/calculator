9fea49ba3a7f5b2709159b1adef731c621dcf39b
Jeroen Ophof <j.ophof@gmail.com>
2024-02-08 09:49:32 +0100

    Added basic button javascript functions Numbers now show up on the screen and the clear button clears it. The display variable stores the screen value for later use.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 let fNum,
     sNum,
-    operator;
+    operator,
+    display = '';
+
+
+const screen = document.querySelector('#calculatorScreen');
+const numberButtons = document.querySelectorAll('#calculatorButtons button.number');
+const clearButton = document.querySelector('#clear');
+const calculateButton = document.querySelector('#calculate');
 
 const add = function() {
 
@@ -21,3 +28,42 @@ const divide = function() {
 const operate = function(fNum, sNum, operator) {
 
 };
+
+const updateScreen = function(value) {
+
+}
+
+const addnumber = function(value) {
+    console.log(`Add number: ${value}`);
+    updateDisplay(value);
+}
+
+const clear = function () {
+    display = '0';
+    screen.textContent = '0';
+}
+
+const calculate = function() {
+    console.log('Calculate');
+}
+
+const updateDisplay = function(value) {
+    if (display === '0' && value === '0') return
+    display  = (display === '0') ? value : display + value;
+    screen.textContent = display;
+}
+
+// add eventlisteners to buttons
+numberButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        addnumber(button.textContent);
+    });
+});
+
+clearButton.addEventListener('click', (e) => {
+    clear();
+});
+
+calculateButton.addEventListener('click', (e) => {
+    calculate();
+});
