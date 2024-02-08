888f28a6827eafa17e410c4c906e5ee35e2f4317
Jeroen Ophof <j.ophof@gmail.com>
2024-02-08 11:35:25 +0100

    Finished basic calculator functionality Reworked the operator buttons and styling a bit to make it cleaner and added all basic javascript functionality.

--- javascript.js
+++ javascript.js
@@ -1,56 +1,69 @@
-let fNum,
-    sNum,
-    operator,
-    display = '';
+let fNum = null,
+    sNum = null,
+    operator = null,
+    display = '0';
 
 
 const screen = document.querySelector('#calculatorScreen');
 const numberButtons = document.querySelectorAll('#calculatorButtons button.number');
+const operatorButtons = document.querySelectorAll('#calculatorButtons button.operator');
 const clearButton = document.querySelector('#clear');
 const calculateButton = document.querySelector('#calculate');
 
-const add = function() {
-
+const add = function(a, b) {
+    return +a + +b;
 };
 
-const subtract = function() {
-
+const subtract = function(a, b) {
+    return +a - +b;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+    return +a * +b;
 };
 
-const divide = function() {
-
+const divide = function(a, b) {
+    return +a / +b;
 };
 
 const operate = function(fNum, sNum, operator) {
-
+    let result;
+    switch(operator) {
+        case 'add':
+            result = add(fNum, sNum);
+            break;
+        case 'subtract':
+            result = subtract(fNum, sNum);
+            break;
+        case 'multiply':
+            result = multiply(fNum, sNum);
+            break;
+        case 'divide':
+            result = divide(fNum, sNum);
+            break;
+        
+    }
+    clear();
+    updateScreen(result);
 };
 
-const updateScreen = function(value) {
-
+const updateScreen = function(value = '0') {
+    //if (display === '0' && value === '0') return
+    display = (display === '0') ? value : display + value;
+    screen.textContent = display;
 }
 
 const addnumber = function(value) {
     console.log(`Add number: ${value}`);
-    updateDisplay(value);
+    updateScreen(value);
 }
 
 const clear = function () {
     display = '0';
-    screen.textContent = '0';
-}
-
-const calculate = function() {
-    console.log('Calculate');
-}
-
-const updateDisplay = function(value) {
-    if (display === '0' && value === '0') return
-    display  = (display === '0') ? value : display + value;
-    screen.textContent = display;
+    updateScreen();
+    operator = null;
+    fNum = null;
+    sNum = null;
 }
 
 // add eventlisteners to buttons
@@ -60,10 +73,23 @@ numberButtons.forEach((button) => {
     });
 });
 
-clearButton.addEventListener('click', (e) => {
-    clear();
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        operator = button.dataset.operator;
+        fNum = display;
+        display ='0';
+        updateScreen(display);
+    });
 });
 
-calculateButton.addEventListener('click', (e) => {
-    calculate();
+clearButton.addEventListener('click', clear);
+
+calculateButton.addEventListener('click', () => {
+    if (fNum) {
+        console.log('operate');
+        sNum = display;
+        operate(fNum, sNum, operator);
+    } else {
+        clear();
+    }
 });
